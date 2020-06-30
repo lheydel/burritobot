611d0636ffecb1f1cc31327d3bf12c408d08fb22
Louec <luc.heydel@gmail.com>
2020-06-30 21:30:30 +0200

    add random OK

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const Discord = require('discord.js')
 
-const OUI = '593794204598272001'
-const NON = '595161673438855168'
-const MAYBE = '596709528280629267'
+const OUI = '<:oui:593794204598272001>'
+const NON = '<:non:595161673438855168>'
+const MAYBE = '<:maybe:596709528280629267>'
+
+const OK = '<:OK:369541026404237312>'
+const Ok = '<:Ok:655871904548126741>'
+
+const PD = '<:PD:513834875418312723>'
+const SAD = '<:sad:369883690479648768>'
 
 const bot = new Discord.Client()
 bot.login(process.env.BOT_TOKEN)
@@ -15,20 +21,21 @@ bot.on('message', async (message) => {
   const content = message.content
   const channel = message.channel
 
-  if (content.endsWith('?') && Math.random() <= 0.1) {
-    const rdm = Math.random()
-    rdm <= 0.495 ? message.react(OUI)
-      : rdm <= 0.99 ? message.react(NON)
-      : message.react(MAYBE)
-  }
-
-  // if (content.includes('alors')) {
-  //   channel.send('<:sad:369883690479648768>')
-  // }
-
   if (content[0] !== '!') {
+    if (content.endsWith('?') && Math.random() <= 0.05) {
+      const rdm = Math.random()
+      rdm <= 0.495 ? channel.send(OUI)
+        : rdm <= 0.99 ? channel.send(NON)
+        : channel.send(MAYBE)
+    }
+
+    if (Math.random() <= 0.002) {
+      channel.send(Math.random <= 0.5 ? OK : Ok)
+    }
+
     return
   }
+
   const [cmd, ...textArray] = content.substring(1).split(' ')
   const text = textArray.join(' ')
   
@@ -42,11 +49,11 @@ bot.on('message', async (message) => {
       break
     case 'ok': 
       message.delete()
-      channel.send('<:OK:369541026404237312>')
+      channel.send(OK)
       break
     case 'pd':
       message.delete()
-      channel.send('<:PD:513834875418312723>')
+      channel.send(PD)
       break;
     // case 'spam':
     //   await new Promise(r => setTimeout(r, (Math.floor(Math.random() * 15) + 30) * 1000));
