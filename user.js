89ca0a89ccb28cc34b68c9ca08e684bb2c7b3dba
Luc Heydel <luc.heydel@gmail.com>
2024-02-18 10:49:24 +0100

    fix bot id on different servers

--- user.js
+++ user.js
@@ -19,17 +19,24 @@ class User {
 const HYRLOS = new User('168333966716174337', CLOCHETTE)
 const LOUEC = new User('160483264761430016', BRINDILLE)
 const MROHMS = new User('366581141118910464', BOB)
-const BOT = new User('726715798676176947', BURRITAL)
+const BOT_SOLO = new User('726715798676176947', BURRITAL)
+const BOT_BTEAM = new User('726765421495320631', BURRITAL)
 
 function getUserById(id) {
-    return [HYRLOS, LOUEC, MROHMS, BOT].find(user => user.id === id)
+    return [HYRLOS, LOUEC, MROHMS, BOT_SOLO, BOT_BTEAM].find(user => user.id === id)
+}
+
+function isBot(string) {
+    return [BOT_SOLO, BOT_BTEAM, 'burritobot', 'bot'].some(user => user.toString() === string)
 }
 
 module.exports = { 
     HYRLOS,
     LOUEC,
     MROHMS,
-    BOT,
+    BOT_SOLO,
+    BOT_BTEAM,
 
-    getUserById
+    getUserById,
+    isBot
 }
